80fa589b2575b3e8caf6c423ff8d030d240ce515
emir <emir.jumaliev@gmail.com>
2019-12-14 04:11:10 -0500

    проект был улучшен

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -9,6 +9,8 @@ class Photo(models.Model):
     likes = models.IntegerField(default=0, verbose_name='Лайки')
     author = models.ForeignKey(User,related_name='photo_author',on_delete=models.PROTECT,
                                     verbose_name='Автор',null=False,blank=False)
+    def __str__(self):
+        return self.label
 
 class Comment(models.Model):
     text = models.CharField(max_length=2000, null=False, blank=False, verbose_name='Текст')
@@ -17,4 +19,15 @@ class Comment(models.Model):
     author = models.ForeignKey(User,related_name='comment_author',on_delete=models.PROTECT,
                                     verbose_name='Автор',null=False,blank=False)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.text
+
+
+class PhotoLike(models.Model):
+    photo = models.ForeignKey('webapp.Photo',related_name='photo_liked',on_delete=models.PROTECT,
+                                    verbose_name='Понравившаяся',null=False,blank=False)
+    owner = models.ForeignKey(User,related_name='photo_like_owner',on_delete=models.PROTECT,
+                                    verbose_name='Хозяин лайка',null=False,blank=False)
+
 # Create your models here.
